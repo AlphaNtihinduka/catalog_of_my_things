fc88de5f3bd90f8f90637d32ba041036ed15f462
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-10-18 23:22:41 +0300

    Add item to items array

--- label.rb
+++ label.rb
@@ -1,16 +1,16 @@
 class Label
-    attr_accessor :title, :color
-    attr_reader :items, :id
+  attr_accessor :title, :color
+  attr_reader :items, :id
 
-    def initialize(title, color)
-        @id = Random.rand(1..1000)
-        @title = title
-        @color = color
-        @items = []
-    end
+  def initialize(title, color)
+    @id = Random.rand(1..1000)
+    @title = title
+    @color = color
+    @items = []
+  end
 
-    def add_item(item)
-        @itmes << item unless @items.include?(item)
-        item.label = self
-    end
+  def add_item(item)
+    @items << item unless @items.include?(item)
+    item.label = self
+  end
 end
