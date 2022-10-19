d21781670cc9a77c6e1b646739437a7d6c6c1dc2
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-10-19 20:16:52 +0300

    Add book method

--- book.rb
+++ book.rb
@@ -12,4 +12,19 @@ class Book < Item
   def can_be_archieved?
     super || @cover_state == 'bad'
   end
+
+  def self.add_book(books)
+    puts 'Add a book'
+    print 'Publish Date: '
+    publish_date = gets.chomp
+    print 'Publisher: '
+    publisher = gets.chomp
+    print 'Cover state Date: '
+    cover_state = gets.chomp
+    books << Book.new(publish_date, publisher, cover_state)
+    puts '💥A book is added successfullly'
+    puts ''
+  end
+end
+
 end
