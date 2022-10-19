3ff5e8068e52b42ae25e5d12aca334741006591b
unknown <tyaps002@gmail,com>
2022-10-19 17:06:32 +0100

    Fix linters

--- genre.rb
+++ genre.rb
@@ -1,14 +1,17 @@
 class Genre
-    attr_reader :name, id
+  attr_reader :name, :id
 
-    def initialize(id, name)
-        @id = Random.rand(1..1000)
-        @name = name
-        @items = []
-    end
-    
-    def add_item(item)
-        @items << item
-        item.add_genre(self)
-    end
+  def initialize(name, _id)
+    @id = Random.rand(1..1000)
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.add_genre(self)
+  end
 end
+
+genre = Genre.new('Titi', 10)
+puts genre.id
