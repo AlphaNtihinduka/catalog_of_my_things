04dc3adf1fedb43b022d70ccc96db528c4e27b6e
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-10-19 20:26:04 +0300

    Define the user prompt options

--- app.rb
+++ app.rb
@@ -7,4 +7,14 @@ class App
     @books = []
     @labels = []
   end
+
+  def options
+    'Do you want to choose an option in the menu? Please type an option number:
+    1. List all books
+    2. List all labels
+    3. Add a book
+    4. Add a label
+    5. Exit
+    Please choose an option:'
+  end
 end
