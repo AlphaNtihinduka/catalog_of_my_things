fb25c6b3b14cebb43953ac4c9382791c31395018
unknown <tyaps002@gmail,com>
2022-10-19 22:53:48 +0100

    Fix linters

---
+++ schema.sql
@@ -0,0 +1,7 @@
+/* Create Music_album table */
+CREATE TABLE music_album(
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    on_spotify BOOLEAN NOT NULL,
+    publish_date DATE NOT NULL,
+    PRIMARY KEY(id)
+)
--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -1,3 +1,5 @@
+require_relative './music_album'
+
 describe MusicAlbum do
   it 'creates a new music album instance' do
     expect(music_album.on_spotify).to eq(true)
