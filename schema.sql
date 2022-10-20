4ee3dc0d7eeed5e16f377b84006ca44ed57db9a5
unknown <tyaps002@gmail,com>
2022-10-20 17:30:11 +0100

    Create genre table in schema

--- schema.sql
+++ schema.sql
@@ -2,6 +2,13 @@
 CREATE TABLE music_album(
     id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
     on_spotify BOOLEAN NOT NULL,
-    publish_date DATE NOT NULL
-    PRIMARY KEY(id)
-)
+    publish_date DATE NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_genre FOREIGN KEY(genre_id) REFERENCES genre(id)
+);
+
+CREATE TABLE genre (
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name varchar(250) NOT NULL,
+    PRIMARY KEY (id)
+);
