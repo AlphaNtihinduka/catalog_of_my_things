7484c2539aaac98bc1bd31943fbd0f69f7d3ddbf
unknown <tyaps002@gmail,com>
2022-10-21 22:57:43 +0100

    Test for all methods for genre

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -5,4 +5,9 @@ describe Genre do
     genre = Genre.new('12 December 2021')
     expect(genre).to be_an_instance_of(Genre)
   end
+
+  it 'should be equal to the name' do
+    genre = Genre.new('Aneal')
+    expect(genre.name).to eq('Aneal')
+  end
 end
