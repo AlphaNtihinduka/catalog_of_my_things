81f464a9218287e285dab5d7bae13fc2906d74d1
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-10-22 09:58:16 +0300

    Refactor Label class

--- label.rb
+++ label.rb
@@ -13,27 +13,4 @@ class Label
     @books << book unless @books.include?(book)
     book.label = self
   end
-
-  # def self.add_label(labels)
-  #   puts 'Add a label'
-  #   print 'Title: '
-  #   title = gets.chomp
-  #   print 'Color: '
-  #   color = gets.chomp
-  #   labels << Label.new(title, color)
-  #   puts '💥A label is added successfullly'
-  #   puts ''
-  # end
-
-  def self.list_all_labels(books)
-    if books.empty?
-      puts 'The Catalog has no labels'
-    else
-      puts '💥List of all labels:'
-      books.each_with_index do |book, index|
-        puts "[#{index + 1}] Title: #{book.title}, Color: #{book.color}"
-      end
-    end
-    puts ''
-  end
 end
