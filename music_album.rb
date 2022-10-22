03dbc3e5a06d8dd93715df03a00228324d54616f
unknown <tyaps002@gmail,com>
2022-10-22 08:38:33 +0100

    Fix linters

--- music_album.rb
+++ music_album.rb
@@ -2,7 +2,7 @@ require_relative './item'
 require_relative './genre'
 
 class MusicAlbum < Item
-  attr_accessor  :title, :on_spotify
+  attr_accessor :title, :on_spotify
 
   def initialize(title, publish_date, on_spotify)
     super(publish_date)
@@ -13,11 +13,13 @@ class MusicAlbum < Item
   def self.list_all_albums(albums)
     if albums.empty?
       puts 'Oops! no album available '
-      else
-        albums.each_with_index { |album, i| 
-          puts "#{i} Title:\"#{album.title}\", Published_date:\"#{album.publish_date}\", Genre:\"#{album.genre.name}\",  Spotify:\"#{album.on_spotify}\" "
-        }
+    else
+      albums.each_with_index do |album, i|
+        puts "#{i}
+         Title:\"#{album.title}\", Published_date:\"#{album.publish_date}\",
+          Genre:\"#{album.genre.name}\",  Spotify:\"#{album.on_spotify}\" "
       end
+    end
   end
 
   def self.create_music_album(albums, genres)
@@ -29,12 +31,12 @@ class MusicAlbum < Item
     g = gets.chomp
     puts 'Is the music album on spotify? [Y/N]: '
     is_on_spotify = gets.chomp
-    spotify_choice = case is_on_spotify.upcase 
-                  when 'Y'
-                    true
-                  else
-                    false
-                  end
+    spotify_choice = case is_on_spotify.upcase
+                     when 'Y'
+                       true
+                     else
+                       false
+                     end
     genre = Genre.new(g)
     album = MusicAlbum.new(title, publish_date, spotify_choice)
     genre.add_item(album)
@@ -42,6 +44,7 @@ class MusicAlbum < Item
     genres << genre
     puts 'Music Album created succesfully '
   end
+
   def can_be_archived?
     true if super() && @on_spotify
   end
--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -2,17 +2,17 @@ require_relative 'spec_helper'
 
 describe MusicAlbum do
   it 'creates a new music album instance' do
-    music_album = MusicAlbum.new('Freeda','12 December 2021', true)
+    music_album = MusicAlbum.new('Freeda', '12 December 2021', true)
     expect(music_album.on_spotify).to eq(true)
   end
 
   it 'creates a new music album instance' do
-    album = MusicAlbum.new('Freeda','12 December 2021', true)
+    album = MusicAlbum.new('Freeda', '12 December 2021', true)
     expect(album.title).to eq('Freeda')
   end
 
   it 'creates a new music album instance' do
-    music = MusicAlbum.new('Freeda','12 December 2021', true)
+    music = MusicAlbum.new('Freeda', '12 December 2021', true)
     expect(music.publish_date).to eq('12 December 2021')
   end
 end
