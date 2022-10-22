5353823f595afa811e61e45a798d464c88bb3103
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-10-22 09:57:39 +0300

    Add, write and read book

--- app.rb
+++ app.rb
@@ -2,11 +2,12 @@ require './item'
 require './book'
 require './label'
 require_relative './json_handler'
+require './validity'
 
 class App
   def initialize
     @books = []
-    # @labels = []
+    @labels = []
   end
 
   def options
@@ -18,18 +19,12 @@ class App
     Please choose an option:'
   end
 
-  # def save_data
-  #   JsonHandler.write_books(@books)
-  #   JsonHandler.write_labels(@labels)
+  # def read_data
+  #   JsonHandler.read_books(@books)
+  #   JsonHandler.read_labels(@labels)
   # end
 
-  def read_data
-    JsonHandler.read_books(@books)
-    JsonHandler.read_labels(@books)
-  end
-
   def menu
-    read_data
     puts 'Welcome to my catalog'
     puts '💥💥💥💥💥💥💥'
     loop do
@@ -37,17 +32,73 @@ class App
       puts '***********'
       option = gets.chomp.to_i
       case option
-      when 1 then Book.list_all_books(@books)
-      when 2 then Label.list_all_labels(@books)
-      when 3 
-         Book.add_book(@books)
-         JsonHandler.write_books(@books)
-         JsonHandler.write_labels(@books)
+      when 1
+        JsonHandler.read_books
+      when 2
+        JsonHandler.read_labels
+      when 3
+        add_book
       when 4
         # save_data
+        JsonHandler.write_books(@books)
+        JsonHandler.write_labels(@labels)
         exit
       else puts 'Invalid option'
       end
     end
+  end
+
+  def add_book
+    puts 'Add a book'
+    print 'Publish Date[dd/mm/yyyy]: '
+    publish_date = gets.chomp
+    publish_date = DateValidation.get_date(publish_date)
+    print 'Publisher: '
+    publisher = gets.chomp
+    print 'Cover state Date["good" or "bad"]: '
+    cover_state = gets.chomp.downcase
+    new_book = Book.new(publish_date, publisher, cover_state)
+    new_label = add_label
+    new_label.add_book(new_book)
+    @books << new_book
+    @labels << new_label
+    puts '💥A book is added successfullly'
+    puts ''
+  end
+
+  def add_label
+    puts 'Add a label'
+    print 'Title: '
+    title = gets.chomp
+    print 'Color: '
+    color = gets.chomp
+    Label.new(title, color)
+  end
+
+  def list_all_books
+    if @books.empty?
+      puts '💥💥The catalog has no books'
+    else
+      puts '💥List of all books:'
+      @books.each_with_index do |book, index|
+        puts "
+        #{index + 1} Publish_date: #{book.publish_date},
+        Publisher: #{book.publisher},
+        Cover_state: #{book.cover_state}"
+      end
+    end
+    puts ''
+  end
+
+  def list_all_labels
+    if @labels.empty?
+      puts 'The Catalog has no labels'
+    else
+      puts '💥List of all labels:'
+      @labels.each_with_index do |book, index|
+        puts "[#{index + 1}] Title: #{book.title}, Color: #{book.color}"
+      end
+    end
+    puts ''
   end
 end
