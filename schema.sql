aadc10a2b5033d21089b4c36d6c427993cba434d
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-10-22 11:54:08 +0300

    Create table and labels

--- schema.sql
+++ schema.sql
@@ -1,5 +1,24 @@
 CREATE DATABASE my_catalog;
 
+-- Create table called labels
+CREATE TABLE labels (
+	id INT GENERATED ALWAYS AS IDENTITY,
+	title	varchar(50),
+	color	varchar(50)
+)
+
+
+-- Create table called books
+CREATE TABLE books (
+	id INT GENERATED ALWAYS AS IDENTITY UNIQUE,
+	publisher	varchar(50), 
+	cover_state	varchar(5), 
+  publish_date DATE,
+  archived BOOLEAN,
+  label_id INT,
+  CONSTRAINT fk_label_id FOREIGN KEY (label_id) REFERENCES label(id),
+)
+
 -- Author table
 CREATE TABLE author (
     id INT GENERATED ALWAYS AS IDENTITY,
